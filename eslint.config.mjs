00bf92a0647cfcbc32c9fb4645cfb9e67e8ced4d
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-17 21:47:59 +0300

    added eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,9 @@ const eslintConfig = [
   ...compat.config({
     extends: ["next"],
     rules: {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   }),
 ];
