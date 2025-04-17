2d4d2b4e0acdd1081c72bc166144bc39817e92a2
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-17 22:27:37 +0300

    fixed issue with tablet and mobile display

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { Card, CardHeader } from "@heroui/react";
+
 export default function Home() {
-  return;
+  return (
+    <div className="mt-10 flex justify-center">
+      <Card className="max-w-[400px]">
+        <CardHeader className="flex gap-3">
+          <div className="flex flex-col">
+            <p className="text-md">Please choose a page you want to visit</p>
+          </div>
+        </CardHeader>
+      </Card>
+    </div>
+  );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -15,34 +15,38 @@ interface ITable {
 
 export const TableComponent = ({ data = [], handleFetch }: ITable) => {
   return (
-    <Table aria-label="Example static collection table">
-      <TableHeader>
-        <TableColumn>ID</TableColumn>
-        <TableColumn>NAME</TableColumn>
-        <TableColumn>PICTURE</TableColumn>
-        <TableColumn>LIKES</TableColumn>
-        <TableColumn>ACTIONS</TableColumn>
-      </TableHeader>
-      <TableBody>
-        {data?.map(({ id, name, picture, likes }) => (
-          <TableRow key={id}>
-            <TableCell>{id}</TableCell>
-            <TableCell>{name}</TableCell>
-            <TableCell>{picture}</TableCell>
-            <TableCell>{likes}</TableCell>
-            <TableCell>
-              <Modal
-                title={`Edit "${name}" meme`}
-                id={id}
-                name={name}
-                picture={picture}
-                likes={likes}
-                handleFetch={handleFetch}
-              />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className="flex justify-center">
+      <Table>
+        <TableHeader>
+          <TableColumn>ID</TableColumn>
+          <TableColumn>NAME</TableColumn>
+          <TableColumn>
+            <div className="max-w-52 truncate">PICTURE</div>
+          </TableColumn>
+          <TableColumn>LIKES</TableColumn>
+          <TableColumn>ACTIONS</TableColumn>
+        </TableHeader>
+        <TableBody>
+          {data?.map(({ id, name, picture, likes }) => (
+            <TableRow key={id}>
+              <TableCell>{id}</TableCell>
+              <TableCell>{name}</TableCell>
+              <TableCell className="max-w-52 truncate">{picture}</TableCell>
+              <TableCell>{likes}</TableCell>
+              <TableCell>
+                <Modal
+                  title={`Edit "${name}" meme`}
+                  id={id}
+                  name={name}
+                  picture={picture}
+                  likes={likes}
+                  handleFetch={handleFetch}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
