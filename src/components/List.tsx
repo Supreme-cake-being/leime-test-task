7c51b53e22e432356f9838f70e1b675ae22fbef0
Karr4 but from another universe <XGon20987@gmail.com>
2025-04-17 22:44:47 +0300

    added link to the card

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,10 @@
-import { Card, CardBody, CardHeader, Image } from "@heroui/react";
+import { Card, CardBody, CardHeader, Image, Link } from "@heroui/react";
 
 interface IList {
   data: Record<string, string | number>[];
 }
 
 export const List = ({ data = [] }: IList) => {
-  console.log(data);
   return (
     <>
       <div className="mt-10 flex justify-center gap-4 flex-wrap">
@@ -14,6 +13,9 @@ export const List = ({ data = [] }: IList) => {
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
               <small className="text-default-500">Likes {likes}</small>
               <h4 className="font-bold text-large">{name}</h4>
+              <Link href={picture as string} target="_blank">
+                Link
+              </Link>
             </CardHeader>
             <CardBody className="overflow-visible py-2">
               <Image
